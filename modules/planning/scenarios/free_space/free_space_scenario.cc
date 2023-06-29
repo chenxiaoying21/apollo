5ead055ec7f50c850473c637d8fc6e60288cb85f
litianjiao <litianjiao@baidu.com>
2023-06-29 16:05:51 +0800

    fix(planning): Determine start/end box has overlap with ROI before transfer scenario
    Change-Id: Ie2853539d2183dba91ea89d20a9fd4f72656ae4b

--- modules/planning/scenarios/free_space/free_space_scenario.cc
+++ modules/planning/scenarios/free_space/free_space_scenario.cc
@@ -20,6 +20,7 @@
 
 #include <vector>
 #include "modules/planning/planning_base/common/frame.h"
+#include "modules/planning/planning_base/common/util/print_debug_info.h"
 #include "modules/planning/planning_base/open_space/utils/open_space_roi_util.h"
 #include "modules/planning/scenarios/free_space/free_space_scenario.h"
 #include "modules/planning/scenarios/free_space/stage_free_space.h"
@@ -56,11 +57,9 @@ bool FreeSpaceScenario::IsTransferable(const Scenario* const other_scenario,
                                        const Frame& frame) {
   const auto& planning_command = frame.local_view().planning_command;
   if (!planning_command->has_custom_command()) {
-    AINFO << "no cmd" << planning_command->DebugString();
     return false;
   }
   if (!planning_command->custom_command().Is<FreeSpaceCommand>()) {
-    AINFO << "not frsp" << planning_command->DebugString();
     return false;
   }
   context_.free_space_command.Clear();
@@ -70,34 +69,53 @@ bool FreeSpaceScenario::IsTransferable(const Scenario* const other_scenario,
            << planning_command->DebugString();
     return false;
   }
-  double adc_speed = frame.vehicle_state().linear_velocity();
-  const double max_adc_stop_speed = common::VehicleConfigHelper::Instance()
-                                        ->GetConfig()
-                                        .vehicle_param()
-                                        .max_abs_speed_when_stopped();
+  const auto& vehicle_state = frame.vehicle_state();
+  const auto& vehicle_param =
+      common::VehicleConfigHelper::Instance()->GetConfig().vehicle_param();
+  double adc_speed = vehicle_state.linear_velocity();
+  const double max_adc_stop_speed = vehicle_param.max_abs_speed_when_stopped();
   if (std::fabs(adc_speed) > max_adc_stop_speed) {
     AINFO << "adc not stopped" << adc_speed;
     return false;
   }
   apollo::common::math::Polygon2d ego_box(injector_->ego_info()->ego_box());
+  Vec2d vec_to_center((vehicle_param.front_edge_to_center() -
+                       vehicle_param.back_edge_to_center()) /
+                          2.0,
+                      (vehicle_param.left_edge_to_center() -
+                       vehicle_param.right_edge_to_center()) /
+                          2.0);
+  const auto& end_pose = context_.free_space_command.parking_spot_pose();
+  Vec2d position(end_pose.x(), end_pose.y());
+  Vec2d center(position + vec_to_center.rotate(end_pose.heading()));
+
+  Polygon2d end_box(Box2d(center, end_pose.heading(), vehicle_param.length(),
+                          vehicle_param.width()));
+  PrintCurves print_curves;
+  print_curves.AddPoint("ego_box", ego_box.points());
+  print_curves.AddPoint("end_box", end_box.points());
   for (const auto& roi_polygon : context_.free_space_command.parking_roi()) {
     std::vector<Vec2d> polygon_points;
     for (const auto& point : roi_polygon.point()) {
       polygon_points.emplace_back(point.x(), point.y());
     }
+    print_curves.AddPoint("free_space_roi", polygon_points);
     Polygon2d polygon_roi(polygon_points);
-    if (OpenSpaceRoiUtil::IsPolygonClockwise(polygon_points)) {
-      if (!polygon_roi.Contains(ego_box)) {
-        AERROR << "ego box is out of roi";
+    if (!OpenSpaceRoiUtil::IsPolygonClockwise(polygon_points)) {
+      if (!polygon_roi.Contains(ego_box) || !polygon_roi.Contains(end_box)) {
+        AERROR << "ego box or end box is out of roi";
+        print_curves.PrintToLog();
         return false;
       }
     } else {
-      if (polygon_roi.HasOverlap(ego_box)) {
-        AERROR << "ego box has collision with obs";
+      if (polygon_roi.HasOverlap(ego_box) || polygon_roi.HasOverlap(end_box)) {
+        AERROR << "ego box or end box has collision with obs";
+        print_curves.PrintToLog();
         return false;
       }
     }
   }
+  print_curves.PrintToLog();
   return true;
 }
 
